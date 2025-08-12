9a98a74acf5ca8fc85c16f915519e2b76fec9470
Parth Kapoor <parth.kapoor@easelearnai.in>
2025-08-12 07:49:47 +0000

    in the logo too make the smiley and the eye of the logo drawn in electri

--- src/components/icons/Logo.tsx
+++ src/components/icons/Logo.tsx
@@ -12,8 +12,8 @@ const Logo = (props: SVGProps<SVGSVGElement>) => (
     {...props}
   >
     <path d="M12 2a10 10 0 1 0 10 10H12V2z" fill="hsl(var(--primary))" stroke="none" />
-    <path d="M12,12 A10,10 0 0 1 2,12" />
-    <circle cx={16} cy={8} r={1} fill="hsl(var(--primary-foreground))" />
+    <path d="M12,12 A10,10 0 0 1 2,12" stroke="hsl(var(--search-ring))" />
+    <circle cx={16} cy={8} r={1} fill="hsl(var(--search-ring))" stroke="none" />
   </svg>
 );
 export default Logo;
