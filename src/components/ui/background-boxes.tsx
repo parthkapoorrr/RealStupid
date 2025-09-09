240aec5b44ba359f17b4c5185c0ec766bc1441c5
Parth Kapoor <parth.kapoor@easelearnai.in>
2025-09-09 20:13:39 +0000

    Try fixing this error: `Runtime Error: Error: A is not defined. Error so

--- src/components/ui/background-boxes.tsx
+++ src/components/ui/background-boxes.tsx
@@ -5,7 +5,7 @@ import { cn } from "@/lib/utils";
 
 export const BoxesCore = ({ className, ...rest }: { className?: string }) => {
   const rows = new Array(150).fill(1);
-  const cols = new A<ctrl61>rray(100).fill(1);
+  const cols = new Array(100).fill(1);
   let colors = [
     "--sky-300",
     "--pink-300",
